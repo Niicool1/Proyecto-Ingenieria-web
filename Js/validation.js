a11140440b6102bf6e033d63d873b4914d3ef8f5
vichosty <vicho.yan@gmail.com>
2023-04-22 21:51:08 -0400

    sass styles y bootstrap para los formularios

---
+++ Js/validation.js
@@ -0,0 +1,7 @@
+const email = document.getElementById('email')
+const contrasenya = document.getElementById('contrasenya')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
